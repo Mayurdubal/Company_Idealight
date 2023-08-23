b96753eb9c150cdab6f8d09e70506429e5d0e789
Prasath Mani <prasath.mani@rubrik.com>
2023-08-23 10:48:50 +0530

    add footer component

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,78 +1,76 @@
 const Footer = () => {
   return (
-    <footer className="gap no-bottom" style={{ backgroundColor: "#222" }}>
+    <footer className="gap no-bottom" style={{ backgroundColor: 'rgba(3, 137, 86, 0.07)' }}>
       <div className="container">
         <div className="row">
           <div className="col-xl-3 col-lg-4 col-md-6">
             <div className="footer-logo">
               <a href="index.html">
-                <img alt="img" src="assets/img/logo.png" />
+                <img alt="img" src="assets/img/idealite-logo.svg" />
               </a>
-              <p>
-                Aliquam porttitor mauris sit amet orci. Aenean dignissim
-                pellentesque felis. Morbi in sem quis dui placerat ornare.
+              <p>Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Cras Bibendum Auctor Justo, Ac Bibendum Neque Sollicitudin Ac.</p>
+              <p className="pt-10">
+                <a href="tel:0123456789">0123456789</a>
               </p>
             </div>
           </div>
           <div className="col-xl-3 col-lg-4 col-md-6">
             <div className="links">
-              <h6>Additional links</h6>
+              <h6 className="txt-green">Explore</h6>
               <ul>
                 <li>
-                  <a href="about.html">About us</a>
+                  <a href="/">Privacy Policy</a>
                 </li>
                 <li>
-                  <a href="services.html">Services</a>
-                </li>
-                <li>
-                  <a href="team.html">team</a>
-                </li>
-                <li>
-                  <a href="blog-grid.html">News</a>
+                  <a href="/">Where to Find Us</a>
                 </li>
                 <li className="pb-0">
-                  <a href="contacts.html">Contact us</a>
+                  <a href="contacts">Contact us</a>
                 </li>
               </ul>
             </div>
           </div>
           <div className="col-xl-3 col-lg-4 col-md-6">
-            <div className="latest-news">
-              <h6>Latest news</h6>
+            <div className="links">
+              <h6 className="txt-green">Company</h6>
               <ul>
                 <li>
-                  <p>
-                    Quisque volutpat mattis eros. Nullam malesuadarat ut turpis.
-                  </p>
-                  <span>dec 10, 2019 / 0 comments</span>
+                  <a href="/about">About Us</a>
                 </li>
-                <li className="pt-4">
-                  <p>Aliquam porttitor mauris sit amet orci.</p>
-                  <span>dec 10, 2019 / 0 comments</span>
+                <li>
+                  <a href="/portfolio">Work</a>
+                </li>
+                <li>
+                  <a href="/services">Services</a>
+                </li>
+                <li className="pb-0">
+                  <a href="blogs">Blogs</a>
                 </li>
               </ul>
             </div>
           </div>
           <div className="col-xl-3 col-lg-4 col-md-6">
-            <div className="latest-news">
-              <h6>Subscribe</h6>
-              <p>
-                Pellentesque odio nisi, euismod in, pharetra a, ultricies in,
-                diam.
-              </p>
-              <form>
-                <input
-                  type="text"
-                  name="email"
-                  placeholder="your email address"
-                />
-                <button>go</button>
-              </form>
+            <div className="links">
+              <h6 className="txt-green">Solutions</h6>
+              <ul>
+                <li>
+                  <a href="/services">App Development</a>
+                </li>
+                <li>
+                  <a href="/services">Web Development</a>
+                </li>
+                <li>
+                  <a href="/services">Cloud Services</a>
+                </li>
+                <li className="pb-0">
+                  <a href="services">Automation Testing</a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
         <div className="footer-bottom">
-          <p>© 2023 Terio - All Rights Reserved | Developed by bslthemes</p>
+          <p>© 2023 Idealite Technologies LLP.</p>
           <ul>
             <li>
               <a href="#">
